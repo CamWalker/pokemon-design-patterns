b6c419bdef8d83b4b40ed81fad48f99bbb03b438
Cameron Walker <whatiscameronsemail@gmail.com>
2018-07-25 13:35:14 -0600

    created review of constructor and prototype. Set up for teaching module pattern

---
+++ pokemon.js
@@ -0,0 +1,93 @@
+const axios = require('axios');
+
+class Attack {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.type = data.type.name;
+  }
+}
+
+class Pokemon {
+  constructor(data) {
+    this.name = data.name;
+    this.id = data.id;
+    this.type = data.types[0].type.name;
+    this.attacks = {};
+  }
+
+  useAttack(attackId) {
+    if (this.attacks[attackId]) {
+      console.log(`
+        "${this.name}!!!!"
+        
+        ${this.attacks[attackId].type === this.type ? "It's super effective!" : ''}
+      `);
+    } else {
+      console.log(`
+        "${this.name}?"
+        
+        ${this.name} hurt itself in a fit of confusion
+      `);
+    }
+  }
+  
+  async learn(attackId) {
+    return axios.get(`http://pokeapi.co/api/v2/move/${attackId}/`).then(data => {
+      const attack = data.data;
+      if (attack.type.name === 'normal' || attack.type.name === this.type) {
+        this.attacks[attackId] = new Attack(attack);
+        console.log(`${this.name} learned ${attack.name}.`)
+      } else {
+        console.log(`Looks like ${this.name} can't learn ${attack.name}`);
+      }
+      return;
+    });
+  }
+}
+
+// const Attack = function(data) {
+//   this.id = data.id;
+//   this.name = data.name;
+//   this.type = data.type.name;
+// }
+
+// const Pokemon = function(data) {
+//   this.name = data.name;
+//   this.id = data.id;
+//   this.type = data.types[0].type.name;
+//   this.attacks = {};
+// }
+
+// Pokemon.prototype.useAttack = function(attackId) {
+//   if (this.attacks[attackId]) {
+//     console.log(`
+//       "${this.name}!!!!"
+      
+//       ${this.attacks[attackId].type === this.type ? "It's super effective!" : ''}
+//     `);
+//   } else {
+//     console.log(`
+//       "${this.name}?"
+      
+//       ${this.name} hurt itself in a fit of confusion
+//     `);
+//   }
+// }
+
+// Pokemon.prototype.learn = async function(attackId) {
+//   return axios.get(`http://pokeapi.co/api/v2/move/${attackId}/`).then(data => {
+//     const attack = data.data;
+//     if (attack.type.name === 'normal' || attack.type.name === this.type) {
+//       this.attacks[attackId] = new Attack(attack);
+//       console.log(`${this.name} learned ${attack.name}.`)
+//     } else {
+//       console.log(`Looks like ${this.name} can't learn ${attack.name}`);
+//     }
+//     return;
+//   });
+// }
+
+module.exports = {
+  Pokemon,
+}
